b4a9d5d477cc7a14de102d62bd58410aa4e517aa
Alexander Mejia SD <SoftwareDeveloperAlexander@gmail.com>
2021-12-21 13:26:47 -0500

    Cambisoo para subir a producicon

--- config/db.js
+++ config/db.js
@@ -4,10 +4,15 @@ require('dotenv').config({ path: 'variables.env'})
 
 
 // Option 3: Passing parameters separately (other dialects)
-const db = new Sequelize(process.env.BD_NAME,process.env.BD_USER, process.env.BD_PASS, {
+const db = new Sequelize(
+  process.env.BD_NOMBRE,
+  process.env.BD_USER,
+  process.env.BD_PASS, {
   host: process.env.BD_HOST,
   port: process.env.BD_PORT,
   dialect:'mysql',
+ 
+  
   define:{
       timestamps:false
   }
