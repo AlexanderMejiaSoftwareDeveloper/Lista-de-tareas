5a77c05ef65f214e07b198ce79b7fc915f35d881
Alexander Mejia SD <SoftwareDeveloperAlexander@gmail.com>
2021-12-21 14:05:16 -0500

    sin subir variables de entorno  y cambio piuerto

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.use((req, res, next) => {
 app.use('/', routes());
 
 const host = process.env.HOST || '0.0.0.0'
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000
 
 app.listen(port,host, ()=>{
     console.log('El servidor esta funcionando correctamente')
