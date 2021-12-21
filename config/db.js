6a714fb10b70bbd156627071240289de3b3db485
Alexander Mejia SD <SoftwareDeveloperAlexander@gmail.com>
2021-12-21 13:12:54 -0500

    Correcion de de config bd

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@ require('dotenv').config({ path: 'variables.env'})
 // Option 3: Passing parameters separately (other dialects)
 const db = new Sequelize(process.env.BD_NAME,process.env.BD_USER, process.env.BD_PASS, {
   host: process.env.BD_HOST,
+  port: process.env.BD_PORT,
   dialect:'mysql',
   define:{
       timestamps:false
